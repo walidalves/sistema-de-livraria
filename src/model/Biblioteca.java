57da94d63b387a495bf1bac802a4b898562e3e85
i3aYFMp1-S <walidi1@hotmail.com>
2026-06-18 23:23:06 -0300

    feat: add hardcoded initial data to Biblioteca constructor
    Creates Autor and Livro objects with predefined data to initialize
    the library system with sample content on startup:
    - 2 authors: Machado de Assis and Edu Falaschi
    - 3 books: Dom Casmurro, Rebirth and Nova Era

--- src/model/Biblioteca.java
+++ src/model/Biblioteca.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -8,4 +9,22 @@ public class Biblioteca {
     private List<Livro> livros = new ArrayList<>();
     private List<Autor> autores = new ArrayList<>();
     private List<Emprestimo> emprestimos = new ArrayList<>();
+
+
+    public Biblioteca() {
+        Autor autor1 = new Autor(1, "Machado de Assis", LocalDate.of(1839, 6, 21));
+        autores.add(autor1);
+
+        Livro livro1 = new Livro(1, "Dom Casmurro", autor1, true, LocalDate.now(), LocalDate.now());
+        livros.add(livro1);
+
+        Autor autor2 = new Autor(2,"Edu Falaschi",  LocalDate.of(2001, 11, 13));
+        autores.add(autor2);
+
+        Livro livro2 = new Livro(2,"Rebirth",  autor2, true, LocalDate.now(), LocalDate.now());
+        livros.add(livro2);
+
+        Livro livro3 = new Livro(3,"Nova Era",  autor2, true, LocalDate.now(), LocalDate.now());
+        livros.add(livro3);
+    }
 }
